5d4a315010317ca03a8a50bb5a5b62b59f58cf71
sjmignot <sjmignot@gmail.com>
2025-01-01 18:23:28 -0800

    fix

--- src/cmd/hash_object.rs
+++ src/cmd/hash_object.rs
@@ -6,7 +6,7 @@ use std::io::Read;
 pub fn hash_object(file_path: &str) -> anyhow::Result<()> {
     let mut file = File::open(file_path).context("Failed to read file")?;
     let mut buf = vec![];
-    file.read_to_end(&mut buf);
+    file.read_to_end(&mut buf)?;
 
     let go = GitObject {
         object_type: ObjectType::Blob,
@@ -18,8 +18,8 @@ pub fn hash_object(file_path: &str) -> anyhow::Result<()> {
     let hash_folder_name = &hash[..2];
     let hash_file_name = &hash[2..];
     let path = format!(".git/objects/{}/{}", hash_folder_name, hash_file_name);
-    // TODO: zlib compress file
-    go.to_file(&path);
+
+    go.to_file(&path)?;
     println!("{}", hash);
     Ok(())
 }
